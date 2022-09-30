5e0f9ec6c35490b58e6e3f559dd86c277c714e2e
Enes Ergin <enesergin1204@hotmail.com>
2022-09-30 17:37:20 +0200

    Update Kunde.cs

--- AksjeApp1/Models/Kunde.cs
+++ AksjeApp1/Models/Kunde.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace AksjeApp1.Models
 {
     public class Kunde
     {
-        public Kunde()
-        {
-        }
+        public int Id { get; set; }
+        public string Fornavn { get; set; }
+        public string Etternavn { get; set; }
+        public string Mail { get; set; }
+        public string Telefonnr { get; set; }
+        public virtual List<Ordre> Ordre { get; set; }
     }
 }
 
