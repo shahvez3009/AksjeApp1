ac25e863c78f59807023ebbfc7255a23c9875d4d
HarmanS01 <70652644+HarmanS01@users.noreply.github.com>
2022-10-10 16:14:06 +0200

    Controller og Kunde
    Gjort en del endring på controller.
    Lagt inn ny attributt (saldo) i Kunde

--- AksjeApp1/Controllers/AksjeAppController.cs
+++ AksjeApp1/Controllers/AksjeAppController.cs
@@ -16,15 +16,44 @@ namespace AksjeApp1.Controllers
 
 
         //Lagrer kjøp 
-        /*
-         * public async Task<bool> Kjøp(Ordre innOrdre)
+       
+        public async Task<bool> Kjøp(Ordre innOrdre)
         {
+            //Først må vi lage en ny rad i ordretabellen for å lagre ordren som har blitt gjort
             var nyOrdreRad = new Ordre();
 
-            //Dropper Id siden det er autoinkrement. Tror det skal funke
-            nyOrdreRad.Antall = innOrdre.Antall;
-            nyOrdreRad.OrdrePris = _db.Aksje.Pris * innOrdre.Antall;
-        }
-        */
+
+
+            nyOrdreRad.OrdreSum = _db.Aksje.Pris * innOrdre.Antall; //Må ta _db.Aksje.
+                                                                    //Pris siden prisen skal ikke komme inn som iput via
+                                                                    //"kjøpskjema", prisen ligger i databasen
+
+            //(ALT UNDER MÅ I EN IF SETNING SOM SKJEKKER OM KUNDEN HAR NOK PENGER)
+
+
+            nyOrdreRad.Antall = innOrdre.Antall;//Dropper Id siden det er autoinkrement.
+                                                //Tror det skal funke
+
+            //Er 1 eller 2 riktig?
+            nyOrdreRad.Kunde = innOrdre.Kunde; //(1)
+            nyOrdreRad.Kunde.Id = innOrdre.Kunde.Id; //(2)
+            
+
+            nyOrdreRad.Aksje = innOrdre.Aksje; //(1) ?
+            nyOrdreRad.Aksje.ID = innOrdre.Aksje.ID; //(2) ?
+
+            nyOrdreRad.Portfolio = innOrdre.Portfolio; //(1)
+            nyOrdreRad.Portfolio.Id = innOrdre.Portfolio.Id; //(2)
+
+            //Endrer saldoen til kunden
+            nyOrdreRad.Kunde.Saldo = innOrdre.Kunde.Saldo - innOrdre.OrdreSum;
+            
+
+            //Vi må også legge inn info i portofolio (husk også endre saldo til kunden i portofolio)
+
+
+         }
+
+
     }
 }
--- AksjeApp1/Models/Kunde.cs
+++ AksjeApp1/Models/Kunde.cs
@@ -8,6 +8,7 @@ namespace AksjeApp1.Models
         public int Id { get; set; }
         public string Fornavn { get; set; }
         public string Etternavn { get; set; }
+        public int Saldo { get; set; };
         public string Mail { get; set; }
         public string Telefonnr { get; set; }
         public virtual Poststed Poststed { get; set; }
