0b6c9f3cb9256c04ccc25858ff4a80a7b6386950
Enes Ergin <enesergin1204@hotmail.com>
2022-10-21 14:39:44 +0200

    Update kjop.js

--- AksjeApp1/wwwroot/Js/kjop.js
+++ AksjeApp1/wwwroot/Js/kjop.js
@@ -1,13 +1,24 @@
 ﻿$(function () {
     // hent aksje med aksje-id fra url og vis denne i skjemaet
 
-    const id = window.location.search.substring(1);
-    const url = "Aksje/HentEnAksje?" + id;
-    $.get(url, function (aksje) {
-        $("#id").val(aksje.id); // må ha med id inn skjemaet, hidden i html
-        $("aksjeNavn").val(aksje.navn);
-        $("aksjePris").val(aksje.pris);
-        $("aksjeMax").val(aksje.maxAntall);
-        $("aksjeLedige").val(aksje.antallLedige);
+    const aksjeid = window.location.search.substring(1);
+    const brukerid = 1;
+    const urlAksje = "Aksje/HentEnAksje?" + aksjeid;
+    const urlBruker = "Aksje/HentEnBruker?" + brukerid;
+
+    $.get(urlAksje, function (aksje) {
+        $("#aksjeId").val(aksje.aksjeid); // må ha med id inn skjemaet, hidden i html
+        $("#aksjeNavn").val(aksje.navn);
+        $("#aksjePris").val(aksje.pris);
+        $("#aksjeMax").val(aksje.maxAntall);
+        $("#aksjeLedige").val(aksje.antallLedige);
     });
+
+    $.get(urlBruker, function (bruker) {
+        $("#brukerId").val(bruker.brukerid);
+        $("#brukerFornavn").val(bruker.fornavn);
+        $("#brukerEtternavn").val(bruker.etternavn);
+        $("#brukerSaldo").val(bruker.saldo);
+    });
+
 });
